afca12a99fc43e5c6aac77748df43a29c9578fe0
monica kirabo <kirabom@duq.edu>
2022-06-21 13:42:37 -0700

    commit here

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,11 +3,14 @@ import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
-import{BrowserRouter} from "react-router-dom"
+import{BrowserRouter, Route} from "react-router-dom"
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Routes>
+    <Route path="/" element={<App/>}/>
+      </Routes>
         <main>
           {/* YOUR CODE HERE! */}
           <Navbar />
