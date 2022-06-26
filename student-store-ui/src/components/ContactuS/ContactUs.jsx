a09a4515b907fb4661702cb6f220a915f5a806aa
monica kirabo <kirabom@duq.edu>
2022-06-26 12:04:59 -0700

    commit here

--- student-store-ui/src/components/ContactuS/ContactUs.jsx
+++ student-store-ui/src/components/ContactuS/ContactUs.jsx
@@ -4,14 +4,30 @@ import "./ContactUs.css";
 export default function ContactUs() {
   return (
     <div className="ContactUs">
+      <h1>Contact Us</h1>
       <div className="content">
-        <h1>Contact Us</h1>
         <div className="text">
           <p> Email: code@path.org</p>
 
           <p> Phone: 1-800-CODEPATH</p>
 
           <p> Address: 123 Fake Street, San Francisco, CA</p>
+
+          <p>
+            Socials:
+            <a href=" ">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href=" ">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href=" ">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href=" ">
+              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </p>
         </div>
         <div className="media">
           <img
