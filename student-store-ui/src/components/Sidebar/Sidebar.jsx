110c66b97a6ebf5305b23ac98213905ba4a7137e
monica kirabo <kirabom@duq.edu>
2022-07-05 16:52:20 -0700

    commit here

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -11,8 +11,7 @@ export default function Sidebar({
   shoppingCart,
   handleOnCheckoutFormChange,
   handleOnSubmitCheckoutForm,
-  handleAddItemToCart,
-  handleRemoveItemToCart,
+  
   checkoutForm,
   setShoppingCart,
   subtotal,
